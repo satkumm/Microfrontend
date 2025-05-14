65aca0bc5b1a6a9a8858b7eef6d0322a2b23f6c4
sathishkumar <sathishkumar.manickam@ibm.com>
2025-05-14 14:45:46 +0530

    Host app use state value after dispatch by remote app

--- microfrontend/react-shell/src/App.jsx
+++ microfrontend/react-shell/src/App.jsx
@@ -25,14 +25,17 @@
 
 
 import React, { Suspense, lazy } from 'react';
+import { useSelector } from 'react-redux';
 
 // 👇 Dynamically import from remote
 const RemoteApp = lazy(() => import('app1/App'));
 
 export default function App() {
+  const user = useSelector((state) => state.user);
+
   return (
     <div>
-      <h1>React Host App</h1>
+      <h1>React Host App {user?.name}</h1>
       <Suspense fallback={<div>Loading remote...</div>}>
         <RemoteApp user={{ name: 'sathish' }}/>
       </Suspense>
